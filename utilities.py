833e052a22dcb95afd2d42ccd04b02f647326f97
Sebastian Seitz <sebastian.seitz1@hs-augsburg.de>
2022-11-29 13:35:13 +0100

    move sieve to utilities.py

--- utilities.py
+++ utilities.py
@@ -1,11 +1,12 @@
+## sieve to generate a list of primes
 def sieve(n):
     b, p, ps = [True] * (n+1) ,2, []
     for p in range(2,n+1):
         if b[p]:
             ps.append(p)
-            print(p)
             for i in range(p,n+1,p):
                 b[i] = False
     return ps
 
-print(sum(sieve(int(100))))
+if __name__ == "__main__":
+    print(sieve(int(1e8))[-1])
